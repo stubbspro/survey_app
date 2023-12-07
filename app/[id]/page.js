ac790bb755898fc1d6b6f836ba584fb66ee2ef15
Bohdan.K <bohdan.kalenichenko@gmail.com>
2023-12-07 04:09:39 -0500

    init

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,10 +1,17 @@
 'use client';
 import React from 'react';
 import { useParams } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 const SurveyPage = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  return (
+    <div>
+      {id}
+      <Button variant='default'>руддщ</Button>
+      <p className='text-red'>helo</p>
+    </div>
+  );
 };
 
 export default SurveyPage;
